0205b71e28203c377c11a95f2803b3ad763fadcd
tanvihkas88 <tanvihkas88@gmail.com>
2024-05-13 17:32:29 +0100

    homework_week_8

---
+++ src/homework_week_8/programme_21_abstractexample/Rectangle.java
@@ -0,0 +1,13 @@
+package homework_week_8.programme_21_abstractexample;
+
+import java.awt.*;
+
+/**
+ * 2. Create a second class name Rectangle which extends by Shape.
+ */
+public class Rectangle extends Shape {
+    public void draw() {
+        System.out.println("drawing rectangle");
+    }
+}
+
---
+++ src/homework_week_8/programme_22_multilevel_inheritance/Animal.java
@@ -0,0 +1,7 @@
+package homework_week_8.programme_22_multilevel_inheritance;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
